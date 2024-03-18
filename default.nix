93c983a22d82e7b68f4ed76d6ef775c8faf73cab
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-03-18 12:13:26 -0300

    default.nix with node and pnpm

--- default.nix
+++ default.nix
@@ -1,24 +1,10 @@
 let
-    pkgs = (import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.zip";
-        sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
-    }) { });
-    stdenv = pkgs.stdenv;
-in pkgs.mkShell rec {
-    name = "interview";
-    shellHook = ''
-        source .bashrc
-    '';
-    buildInputs = (with pkgs; [
-        bashInteractive
-        (pkgs.python3.buildEnv.override {
-            ignoreCollisions = true;
-            extraLibs = with pkgs.python3.pkgs; [
-                # package list: https://search.nixos.org/packages
-                # be parsimonious with 3rd party dependencies; better to show off your own code than someone else's
-                ipython
-                nose
-            ];
-        })
-    ]);
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+in
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    nodejs-18_x
+    nodePackages.pnpm
+  ];
 }
